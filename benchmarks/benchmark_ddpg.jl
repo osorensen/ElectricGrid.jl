445388f4cc1a30e954efc48f7c65a247bcfaf9e7
cvikas <vikasc.nitk@gmail.com>
2023-05-11 21:18:43 +0200

    #98 add benchmark for RL control with cpu

---
+++ benchmarks/benchmark_ddpg.jl
@@ -0,0 +1,110 @@
+using ElectricGrid
+using BenchmarkTools
+using Distributions
+using JLD2
+using StatsPlots
+
+function GetEnv(num_nodes, source_type = "RL")
+    """
+    Generates an env of source and load nodes for a grid with a given source type
+    """   
+
+    env = ElectricGridEnv(  
+                num_sources = num_nodes, 
+                num_loads = num_nodes, 
+                t_end = 1.0,  #dt = 0.0001
+                verbosity = 0,
+                ) 
+
+
+    for i in 1:num_nodes
+        if source_type == "classical"
+            env.nc.parameters["source"][i]["mode"] = "Swing"
+        end
+        env.nc.parameters["source"][i]["source_type"] = source_type
+        # env.nc.parameters["source"][i]["mode"] = "ddpg"
+    end
+
+    new_env = ElectricGridEnv(
+        CM=env.nc.CM,
+        action_delay=0,
+        t_end = 1.0, 
+        parameters = env.nc.parameters,
+        )
+
+    return new_env
+end
+
+function GetAgent(RLenv)
+    """
+    Generates an agent for a given env
+    """
+    agent = CreateAgentDdpg(na = length(RLenv.action_ids),
+                ns = length(RLenv.state_ids),
+                use_gpu = false);
+
+    custom_agents = Dict("ddpg" => agent)
+
+    controller = SetupAgents(RLenv, custom_agents);
+
+    return controller
+end
+
+function Benchmark(num_nodes, source_type = "RL")
+    """
+    Runs benchmark for the env for a given number of nodes
+    """
+    env = GetEnv(num_nodes, source_type)
+    controller = GetAgent(env)
+
+    b = @benchmark Simulate($controller, $env) samples=3 seconds=60
+    return b
+end
+
+function CollectBenchmarkData(max_num_nodes, delta_num_nodes = 1)
+    """
+    Collects benchmark data for a given number of nodes
+    """
+    # dummy run
+    @info "Dummy run..."
+    Benchmark(2)
+
+    for i in 2:delta_num_nodes:max_num_nodes
+
+        @info "Running benchmark for $i nodes..."
+        b = Benchmark(i)
+        push!(benchmark_data, b)
+    end        
+
+end
+
+# simulation parameter
+max_num_nodes = 50
+delta_num_nodes = 4
+control_type = "RL"
+
+benchmark_data = []
+
+CollectBenchmarkData(max_num_nodes, delta_num_nodes)
+
+@save "benchmark_$(control_type)_$(delta_num_nodes)_$(max_num_nodes).jld2" benchmark_data
+
+# plot benchmark data
+times = [(b.times * 1e-9) for b in benchmark_data]
+nodes = collect(2:delta:max_num_nodes)
+
+StatsPlots.plot(
+            nodes, 
+            mean.(times), 
+            yerr = [std(b) for b in times], 
+            xlabel = "Number of nodes", 
+            ylabel = "Averaged over 3 differnt runs", 
+            label = "Benchmark for RL", #y axis label
+            title = "Benchmark for RL control for 1s (with Δt = 1ms)", 
+            legend = :topright)
+
+
+
+
+
+             
