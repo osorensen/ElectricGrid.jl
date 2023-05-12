3d9fcc23fac654d3f2d0360e65e3d1fc8dc0c321
cvikas <vikasc.nitk@gmail.com>
2023-05-12 16:41:05 +0200

    #98 add test agent_dict

---
+++ benchmarks/test.jl
@@ -0,0 +1,23 @@
+using ElectricGrid
+using BenchmarkTools
+
+nc = NodeConstructor(
+    num_sources = 2,
+    num_loads = 2,
+)
+
+# replace control type to RL and mode to ddpg
+for i in 1:nc.num_sources
+    nc.parameters["source"][i]["source_type"] = "RL"
+    nc.parameters["source"][i]["mode"] = "ddpg"
+end
+
+temp_env = ElectricGridEnv(
+    CM=nc.CM,
+    action_delay=0,
+    t_end = 1.0, 
+    parameters = nc.parameters,
+    )
+
+@show temp_env.nc.parameters["source"][1]["mode"]
+@show temp_env.agent_dict
