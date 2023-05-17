9055d1b0c00556791c3da3c79791bf1f5ec6b516
cvikas <vikasc.nitk@gmail.com>
2023-05-17 17:13:58 +0200

    #98 add ribbons for std in plots

--- benchmarks/plots.jl
+++ benchmarks/plots.jl
@@ -4,44 +4,72 @@ using BenchmarkTools
 using Distributions
 using StatsPlots
 
+# directory for data and plots
 data_dir = pwd() * "/benchmarks/data/"
-classical_data = load(data_dir * "benchmark_lea_1_15.jld2")["benchmark_data"]
-ddpg_data = load(data_dir * "benchmark_lea_RL_1_15.jld2")["benchmark_data"]
+plotsdir = pwd() * "/benchmarks/plots/"
 
-# load Benchmark data from JLD2
-# classical_data = load("benchmark_5_50.jld2")["benchmark_data"]
+# benchmark data
+classical_swing_data = load(data_dir * "benchmark_lea_1_15.jld2")["benchmark_data"]
+ddpg_data = load(data_dir * "benchmark_lea_RL_1_15.jld2")["benchmark_data"]
+classical_VSG_data = load(data_dir * "benchmark_csvm_Synchronverter_1_15.jld2")["benchmark_data"]
+
+
 
 # plot classical data and ddpg data
 nodes = collect(1:1:15)
-times_classical = [(b.times * 1e-9) for b in classical_data]
-times_ddpg = [(b.times * 1e-9) for b in ddpg_data]
 
-# constant time for real time at 1
+times_vsg = [(b.times * 1e-9) for b in classical_VSG_data]
+times_ddpg = [(b.times * 1e-9) for b in ddpg_data]
+time_swing = [(b.times * 1e-9) for b in classical_swing_data]
+
+# constant time for real time at 1s
 real_time = [1 for _ in classical_data]
 
-StatsPlots.plot(nodes, 
-    mean.(times_classical), 
-    yerr = [std(b) for b in times_classical],
-    label="Open loop simulation", 
-    title="Benchmark of Simulation for 1s (with Δt = 1ms)", 
-    xlabel="Number of nodes [equally distributed sources and loads]", 
-    ylabel="Time averaged over 3 benchmarks (s)")
 
-StatsPlots.plot!(
-    nodes,
-    mean.(times_ddpg),
-    yerr = [std(b) for b in times_ddpg],
-    label="RL control")
-# plot realtime
-
-StatsPlots.plot!(
+StatsPlots.plot(
     nodes,
     real_time,
     label="1s baseline",
     linestyle=:dash,
-    color=:black,)
+    color=:black,
+    title="Benchmark of Simulation for 1s (with Δt = 1ms)", 
+    xlabel="Number of nodes [equally distributed sources and loads]", 
+    ylabel="Time averaged over 3 benchmarks (s)"
+    )
 
-savefig("benchmark_lea_1_15.png")
+StatsPlots.plot(nodes, 
+    mean.(times_classical), 
+    ribbon = [std(b) for b in times_classical],
+    label="VSG",
+    fillalpha=0.3, 
+    )
+
+
+StatsPlots.plot!(
+    nodes,
+    mean.(time_swing),
+    fillalpha=0.3,
+    ribbon = [std(b) for b in time_swing],
+    # yerr = [std(b) for b in times_ddpg],
+    label="RL control",
+    color=:red,)
+# plot realtime
+
+
+
+StatsPlots.plot!(
+    nodes,
+    mean.(times_ddpg),
+    ribbon = [std(b) for b in time_swing],
+    label="ddpg",
+    color=:green,
+    fillalpha=0.3,
+    )
+    
+
+
+
+savefig(plotsdir * "benchmark_csvm_swing_vsg_1_15.png")
 # try with plotlyS
 # using PlotlyJS
 
